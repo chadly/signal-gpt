2e82aa4154f6c00d83accd064271437c8efaded0
Chad Lee <git@chadly.net>
2023-04-27 09:38:41 -0500

    ✨ feat: page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import Setup, { SetupForm } from "@/components/setup";
 import Conversation from "@/components/convo";
 import { useState } from "react";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,12 +14,17 @@ const Home = () => {
 	};
 
 	return (
-		<main
-			className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-		>
-			{!setup && <Setup onSubmit={onSubmit} />}
-			{setup && <Conversation setup={setup} />}
-		</main>
+		<>
+			<Head>
+				<title>Signal-GPT</title>
+			</Head>
+			<main
+				className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+			>
+				{!setup && <Setup onSubmit={onSubmit} />}
+				{setup && <Conversation setup={setup} />}
+			</main>
+		</>
 	);
 };
 
